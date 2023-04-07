4a84ad4e430458c42024fdc388ee31febfd24e52
Sumina Hathidara <suminahathidara@suminas-mbp.lan>
2023-04-07 16:21:16 -0400

    few changes

--- static/js/google-maps.js
+++ static/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 43.65980207248801, lng: -79.4402792886272};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Toronto, Canada' // Title Location
     });
 }
